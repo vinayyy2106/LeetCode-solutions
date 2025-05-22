342f5bb409105e086caa3058338d7285e4d5ba87
vinayyy2106 <96223810+vinayyy2106@users.noreply.github.com>
2025-05-22 18:36:01 +0530

    Time: 2 ms (3.47%), Space: 40.8 MB (17.67%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int uniquePaths(int m, int n) {
+        int[][] dp=new int[m][n];
+        System.out.println(m);
+        System.out.println(n);
+        return noOfPaths(dp.length-1,dp[0].length-1,dp);
+    }
+    public int noOfPaths(int i,int j,int[][] dp){
+        if(i==0 && j==0){
+            return 1;
+        }
+        if(i<0 || j<0){
+            return 0;
+        }
+        if(dp[i][j]!=0){
+            return dp[i][j];
+        }
+        int up=noOfPaths(i-1,j,dp);
+        int down=noOfPaths(i,j-1,dp);
+        dp[i][j]=up+down;
+        return dp[i][j];
+    }
+}
