e55ba6253a2265611e18812bb4299776d32bb6c2
vinayyy2106 <96223810+vinayyy2106@users.noreply.github.com>
2025-05-24 16:16:52 +0530

    Time: 2 ms (78.93%), Space: 44.3 MB (49.13%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.java
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.java
@@ -1,13 +1,32 @@
 class Solution {
     public int maxProfit(int k, int[] prices) {
         int n=prices.length;
-        int[][][] dp=new int[n][2][k+1];
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < 2; j++) {
-                Arrays.fill(dp[i][j], -1);
+        int[][] ahead=new int[2][k+1];
+        for(int i=n-1;i>=0;i--){
+            int[][] cur=new int[2][k+1];
+            for(int buy=0;buy<=1;buy++){
+                for(int cap=1;cap<=k;cap++){
+                    int price=0;
+                    if(buy==1){
+                        price=Math.max((-1)*prices[i]+ahead[0][cap],0+ahead[1][cap]);
+                    }
+                    if(buy==0){
+                        price=Math.max(prices[i]+ahead[1][cap-1],0+ahead[0][cap]);
+                    }
+                    cur[buy][cap]=price;
+                }
+                
             }
+            ahead=cur;
         }
-        return maxProf(prices,n,k,dp,1,0);
+        // int[][][] dp=new int[n][2][k+1];
+        // for (int i = 0; i < n; i++) {
+        //     for (int j = 0; j < 2; j++) {
+        //         Arrays.fill(dp[i][j], -1);
+        //     }
+        // }
+        // return maxProf(prices,n,k,dp,1,0);
+        return ahead[1][k];
     }
     public int maxProf(int[] prices,int n,int max,int[][][] dp,int buy,int ind){
         if(max==0){
