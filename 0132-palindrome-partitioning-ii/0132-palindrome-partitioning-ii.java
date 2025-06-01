6a28cc49ccbab0ce4cf2f99b149ce47bfc46e183
vinayyy2106 <96223810+vinayyy2106@users.noreply.github.com>
2025-06-01 12:41:39 +0530

    Time: 2752 ms (9.91%), Space: 41.6 MB (69.35%) - LeetHub

---
+++ 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int minCut(String s) {
+        int n= s.length();
+        int[] dp=new int[n+1];
+        Arrays.fill(dp,-1);
+        return minCount(0,n,dp,s)-1;
+    }
+    public int minCount(int i,int n,int[] dp,String s){
+        if(i==n)return 0;
+        if (dp[i] != -1) return dp[i];
+        int min=Integer.MAX_VALUE;
+        for(int k=i;k<n;k++){
+            if(isPalindrome(i,k,s)){
+                int cost=1+minCount(k+1,n,dp,s);
+                min=Math.min(cost,min);
+            }
+        }
+        return dp[i]=min;
+    }
+    public boolean isPalindrome(int i,int j,String str){
+        while(i<j){
+            if(str.charAt(i)!=str.charAt(j)){
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+}
