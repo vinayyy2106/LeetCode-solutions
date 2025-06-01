41c32083ad1b4b9af2c7519e4d8a044270fcff8f
vinayyy2106 <96223810+vinayyy2106@users.noreply.github.com>
2025-06-01 11:49:52 +0530

    Time: 32 ms (90.97%), Space: 42.7 MB (66.45%) - LeetHub

--- 0312-burst-balloons/0312-burst-balloons.java
+++ 0312-burst-balloons/0312-burst-balloons.java
@@ -8,10 +8,22 @@ class Solution {
         }
         newArr[n+1]=1;
         int[][] dp=new int[n+2][n+2];
-        for(int[] arr:dp){
-            Arrays.fill(arr,-1);
+        for(int i=n;i>=1;i--){
+            for(int j=1;j<=n;j++){
+                if(i>j)continue;
+                int max=Integer.MIN_VALUE;
+                for(int k=i;k<=j;k++){
+                    int ans=dp[i][k-1]+dp[k+1][j]+newArr[i-1]*newArr[k]*newArr[j+1];
+                    max=Math.max(max,ans);
+                }
+                dp[i][j]=max;
+            }
         }
-        return maxValue(1,n,newArr,dp);
+        return dp[1][n];
+        // for(int[] arr:dp){
+        //     Arrays.fill(arr,-1);
+        // }
+        // return maxValue(1,n,newArr,dp);
     }
     public int maxValue(int i,int j,int[] newArr,int[][] dp){
         if(i>j)return 0;
