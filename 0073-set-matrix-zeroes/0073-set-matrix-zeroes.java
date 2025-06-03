e6d30eaa653011b31ae335036734648bcff36a58
vinayyy2106 <96223810+vinayyy2106@users.noreply.github.com>
2025-06-03 19:19:45 +0530

    Time: 1 ms (72.28%), Space: 45.5 MB (91.8%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -2,16 +2,16 @@ class Solution {
     public void setZeroes(int[][] matrix) {
         int n=matrix.length;
         int m=matrix[0].length;
-        int colo=1;
+        int row=1;
         //considering row 0 and col 0 itself as indicators
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(matrix[i][j]==0){
-                    matrix[i][0]=0;
-                    if(j>0){
-                        matrix[0][j]=0;
+                    matrix[0][j]=0;
+                    if(i>0){
+                        matrix[i][0]=0;
                     }else{
-                        colo=0;
+                        row=0;
                     }
                 }
             }
@@ -29,13 +29,13 @@ class Solution {
         }
         //now doing it for rest elements that is we never argeted matrix[0][0] and we need to do colums elements bcz changing wise rows will effect the matrix 1st column
         if(matrix[0][0]==0){
-            for(int j=0;j<m;j++){
-                matrix[0][j]=0;
+            for(int j=0;j<n;j++){
+                matrix[j][0]=0;
             }
         }
-        if(colo==0){
-            for(int i=0;i<n;i++){
-                matrix[i][0]=0;
+        if(row==0){
+            for(int i=0;i<m;i++){
+                matrix[0][i]=0;
             }
         }
     }
