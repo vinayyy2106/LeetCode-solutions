ac1c4af4ad51addbdd22d6e7e6c7692290cc2cdc
vinayyy2106 <96223810+vinayyy2106@users.noreply.github.com>
2025-06-04 19:47:37 +0530

    Time: 0 ms (100%), Space: 42.2 MB (77.54%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -1,23 +1,42 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
         //as discussed in strivers vdeo 
+        // int i=m-1;
+        // int j=0;
+        // while(i!=-1 && j<n){
+        //     if(nums1[i]>nums2[j]){
+        //         int temp=nums1[i];
+        //         nums1[i]=nums2[j];
+        //         nums2[j]=temp;
+        //         i--;
+        //         j++;
+        //     }else{
+        //         break;
+        //         //as arrays are sorted if elements at particular index are as expecte then we can break as other elements would be also at correct place
+        //     }
+        // }
+        // for(int k=0;k<n;k++){
+        //     nums1[m+k]=nums2[k];
+        // }
+        // Arrays.sort(nums1);
         int i=m-1;
-        int j=0;
-        while(i!=-1 && j<n){
+        int j=n-1;
+        int k=m+n-1;
+        while(i>=0 && j>=0){
             if(nums1[i]>nums2[j]){
-                int temp=nums1[i];
-                nums1[i]=nums2[j];
-                nums2[j]=temp;
+                nums1[k]=nums1[i];
                 i--;
-                j++;
+                k--;
             }else{
-                break;
-                //as arrays are sorted if elements at particular index are as expecte then we can break as other elements would be also at correct place
+                nums1[k]=nums2[j];
+                k--;
+                j--;
             }
         }
-        for(int k=0;k<n;k++){
-            nums1[m+k]=nums2[k];
+        while(j>=0){
+            nums1[k]=nums2[j];
+            j--;
+            k--;
         }
-        Arrays.sort(nums1);
     }
 }
