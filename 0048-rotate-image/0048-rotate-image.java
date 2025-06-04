312ba8d06c24d5357eb05b3f08b041a4db6e8150
vinayyy2106 <96223810+vinayyy2106@users.noreply.github.com>
2025-06-04 18:29:47 +0530

    Time: 0 ms (100%), Space: 42.3 MB (42.1%) - LeetHub

---
+++ 0048-rotate-image/0048-rotate-image.java
@@ -0,0 +1,28 @@
+class Solution {
+    public void rotate(int[][] matrix) {
+        int n=matrix.length;
+        for(int i=0;i<n-1;i++){
+            for(int j=i+1;j<n;j++){
+                swap(i,j,i,matrix);
+            }
+        }
+        for(int[] row:matrix){
+            reverseArray(0,row.length-1,row);
+        }
+    }
+
+    public void swap(int i,int j,int k,int[][] matrix){
+        int temp=matrix[i][j];
+        matrix[i][j]=matrix[j][k];
+        matrix[j][k]=temp;
+    }
+    public void reverseArray(int i,int j,int[] arr){
+        while(i<j){
+            int temp=arr[i];
+            arr[i]=arr[j];
+            arr[j]=temp;
+            i++;
+            j--;
+        }
+    }
+}
