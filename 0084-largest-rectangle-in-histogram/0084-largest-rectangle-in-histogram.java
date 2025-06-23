324c8bef2ce0c6087ff6e3bf5f28219644a5ddb3
vinayyy2106 <96223810+vinayyy2106@users.noreply.github.com>
2025-06-23 13:32:19 +0530

    Time: 120 ms (8.14%), Space: 57.5 MB (53.09%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
@@ -1,18 +1,20 @@
 class Solution {
     public int largestRectangleArea(int[] heights) {
+        int n=heights.length;
         int max=0;
-        int[] rightSmallest=new int[heights.length];
-        int[] leftSmallest=new int[heights.length];
+        int[] rightSmallest=new int[n];
+        int[] leftSmallest=new int[n];
         findleftSmallest(leftSmallest,heights);
         findrightSmallest(rightSmallest,heights);
-        for(int i=0;i<heights.length;i++){
+        for(int i=0;i<n;i++){
             max=Math.max(max,(rightSmallest[i]-leftSmallest[i]-1)*heights[i]);
         }
         return max;
     }
     public void findleftSmallest(int[] left,int[] heights){
+        int n=heights.length;
         Stack<Integer> st=new Stack<>();
-        for(int i=0;i<heights.length;i++){
+        for(int i=0;i<n;i++){
             while(!st.isEmpty() && heights[st.peek()]>=heights[i]){
                 st.pop();
             }
@@ -23,8 +25,9 @@ class Solution {
         }
     }
     public void findrightSmallest(int[] right,int[] heights){
+        int n=heights.length;
         Stack<Integer> st=new Stack<>();
-        for(int i=heights.length-1;i>=0;i--){
+        for(int i=n-1;i>=0;i--){
             while(!st.isEmpty() && heights[st.peek()]>=heights[i]){
                 st.pop();
             }
