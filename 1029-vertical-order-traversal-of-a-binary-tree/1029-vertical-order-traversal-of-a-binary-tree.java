4a3cd8fc933dc7e73297710dc85744a32ea23025
vinayyy2106 <96223810+vinayyy2106@users.noreply.github.com>
2025-07-02 19:38:17 +0530

    Time: 4 ms (42.78%), Space: 42.5 MB (66.87%) - LeetHub

---
+++ 1029-vertical-order-traversal-of-a-binary-tree/1029-vertical-order-traversal-of-a-binary-tree.java
@@ -0,0 +1,63 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> verticalTraversal(TreeNode root) {
+        TreeMap<Integer,TreeMap<Integer,PriorityQueue<Integer>>> mp=new TreeMap<>();
+        Queue<Tuple> q=new LinkedList<>();
+        q.offer(new Tuple(root,0,0));
+        while(!q.isEmpty()){
+            Tuple tp=q.poll();
+            TreeNode nd=tp.node;
+            int row=tp.level;
+            int col=tp.order;
+            if(!mp.containsKey(row)){
+                mp.put(row,new TreeMap<>());
+            }
+            if(!mp.get(row).containsKey(col)){
+                mp.get(row).put(col,new PriorityQueue<>());
+            }
+            mp.get(row).get(col).offer(nd.val);
+            if(nd.left!=null){
+                q.offer(new Tuple(nd.left,row-1,col+1));
+            }
+            if(nd.right!=null){
+                q.offer(new Tuple(nd.right,row+1,col+1));
+            }
+            
+        }
+        List<List<Integer>> list=new ArrayList<>();
+        for(TreeMap<Integer,PriorityQueue<Integer>> a:mp.values()){
+                list.add(new ArrayList<>());
+                for(PriorityQueue<Integer> nodes:a.values()){
+                    while(!nodes.isEmpty()){
+                        list.get(list.size()-1).add(nodes.poll());
+                    }
+                    
+                }
+        }
+        return list;
+    }
+}
+class Tuple{
+    TreeNode node;
+    int level;
+    int order;
+    Tuple(TreeNode node,int level,int order){
+        this.node=node;
+        this.level=level;
+        this.order=order;
+    }
+}
