ddb9e047a8e12c619f14e92af10862ea6cdee9d2
vinayyy2106 <96223810+vinayyy2106@users.noreply.github.com>
2025-07-15 17:03:02 +0530

    Time: 1 ms (99.12%), Space: 42 MB (62.25%) - LeetHub

--- 3396-valid-word/3396-valid-word.java
+++ 3396-valid-word/3396-valid-word.java
@@ -1,13 +1,12 @@
 class Solution {
     public boolean isValid(String word) {
-        Set<Character> charSet1 = new HashSet<>(Arrays.asList('a', 'e', 'i', 'o','u'));
         if(word.length()<3)return false;
         boolean vowel=false;
         boolean consonant=false;
         for(char ch:word.toCharArray()){
             if(Character.isLetter(ch)){
                 ch=Character.toLowerCase(ch);
-                if(charSet1.contains(ch)){
+                if(ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u'){
                     vowel=true;
                 }else{
                     consonant=true;
