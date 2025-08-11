9c8383cbf37b4e06c90c71de9a1e7bde0ac861e6
vinayyy2106 <96223810+vinayyy2106@users.noreply.github.com>
2025-08-11 22:06:58 +0530

    Time: 0 ms (100%), Space: 41.1 MB (49.35%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.java
+++ 0486-predict-the-winner/0486-predict-the-winner.java
@@ -1,17 +1,26 @@
 class Solution {
     public boolean predictTheWinner(int[] nums) {
-        int playerOne=helper(0,nums.length-1,nums);
+        int[][] dp=new int[21][21];
+        for(int[] arr:dp){
+            Arrays.fill(arr,-1);
+        }
+        int playerOne=helper(0,nums.length-1,nums,dp);
         int sum=0;
+        
+        
         for(int n:nums){
             sum+=n;
         }
         return playerOne>=sum-playerOne;
     }
-    public int helper(int i,int j,int[] nums){
+    public int helper(int i,int j,int[] nums,int[][] dp){
         if(i>j)return 0;
         if(i==j)return nums[i];
-        int firstEle=nums[i]+(Math.min(helper(i+2,j,nums),helper(i+1,j-1,nums)));
-        int secondEle=nums[j]+Math.min(helper(i+1,j-1,nums),helper(i,j-2,nums));
-        return Math.max(firstEle,secondEle);
+        if(dp[i][j]!=-1){
+            return dp[i][j];
+        }
+        int firstEle=nums[i]+(Math.min(helper(i+2,j,nums,dp),helper(i+1,j-1,nums,dp)));
+        int secondEle=nums[j]+Math.min(helper(i+1,j-1,nums,dp),helper(i,j-2,nums,dp));
+        return dp[i][j]=Math.max(firstEle,secondEle);
     }
 }
