e2a70f95b9039e2c32df2025e058a96c17ced90c
vinayyy2106 <96223810+vinayyy2106@users.noreply.github.com>
2025-08-12 10:28:30 +0530

    Time: 326 ms (18.95%), Space: 45.1 MB (43.68%) - LeetHub

--- 2882-ways-to-express-an-integer-as-sum-of-powers/2882-ways-to-express-an-integer-as-sum-of-powers.java
+++ 2882-ways-to-express-an-integer-as-sum-of-powers/2882-ways-to-express-an-integer-as-sum-of-powers.java
@@ -1,15 +1,16 @@
 class Solution {
     final int MOD = 1_000_000_007;
     public int numberOfWays(int n, int x) {
-        int[][] dp=new int[n+1][n+1];
+        int[][] dp=new int[302][302];
         for(int[] arr:dp)Arrays.fill(arr,-1);
         return helper(n,1,x,dp);
     }
     public int helper(int n,int num,int x,int[][] dp){
         if(n==0)return 1;
-        if (num > n) return 0;
-        if(dp[n][num]!=-1)return dp[n][num];
+        // if (num > n) return 0;
         int powValue=(int)Math.pow(num,x);
+        if(dp[n][num]!=-1)return dp[n][num];
+        
         if(powValue>n)return 0;
         int takeNum=helper(n-powValue,num+1,x,dp);
         int skipNum=helper(n,num+1,x,dp);
