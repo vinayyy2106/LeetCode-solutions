9193613458fb33d9bb7683f0492c93f634482366
vinayyy2106 <96223810+vinayyy2106@users.noreply.github.com>
2025-09-04 17:24:52 +0530

    Time: 1 ms (34.71%), Space: 41.5 MB (5.62%) - LeetHub

---
+++ 3830-find-closest-person/3830-find-closest-person.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int findClosest(int x, int y, int z) {
+        if(Math.abs(z-y)<Math.abs(z-x)){
+            return 2;
+        }else if(Math.abs(z-y)>Math.abs(z-x)){
+            return 1;
+        }else{
+            return 0;
+        }
+    }
+}
