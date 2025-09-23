38098cded705d0c033106af391f7c32dc8a9b120
vinayyy2106 <96223810+vinayyy2106@users.noreply.github.com>
2025-09-23 08:50:45 +0530

    Time: 1 ms (76.03%), Space: 41.7 MB (48.13%) - LeetHub

--- 0165-compare-version-numbers/0165-compare-version-numbers.java
+++ 0165-compare-version-numbers/0165-compare-version-numbers.java
@@ -3,8 +3,6 @@ class Solution {
         String[] words_1 = version1.split("\\.");
         String[] words_2 = version2.split("\\.");
         int n=Math.max(words_1.length,words_2.length);
-        // System.out.println(words_1.length);
-        // System.out.println(words_2.length);
         for(int i=0;i<n;i++){
             int v1=i < words_1.length ? Integer.parseInt(words_1[i]) : 0;
             int v2=i < words_2.length ? Integer.parseInt(words_2[i]) : 0;
