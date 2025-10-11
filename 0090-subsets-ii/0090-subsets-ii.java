cafd6cf7c359005926c17b8d01a0c9e7c54b600d
vinayyy2106 <96223810+vinayyy2106@users.noreply.github.com>
2025-10-11 19:28:03 +0530

    Time: 2 ms (28.1%), Space: 43.5 MB (62.64%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.java
+++ 0090-subsets-ii/0090-subsets-ii.java
@@ -1,33 +1,28 @@
 class Solution {
     public List<List<Integer>> subsetsWithDup(int[] nums) {
         Arrays.sort(nums);
-        List<List<Integer>> arrList=new ArrayList<>();
+        List<List<Integer>> li=new ArrayList<>();
         if(nums.length==1){
-            arrList.add(new ArrayList<>());
-            List<Integer> li=new ArrayList<>();
-            li.add(nums[0]);
-            arrList.add(li);
-            return arrList;
+            li.add(new ArrayList<>());
+            List<Integer> r=new ArrayList<>();
+            r.add(nums[0]);
+            li.add(r);
+            return li;
         }else{
-            returnSubsetArray(arrList,0,nums,new ArrayList<Integer>());
-            return arrList;
+            recursion(li,new ArrayList<>(),0,nums);
         }
+        return li;
     }
-    static void returnSubsetArray(List<List<Integer>> arrList,int index,int[] nums,ArrayList<Integer> list){
-       
-        if(index==nums.length){
-            arrList.add(new ArrayList<>(list));
+    public void recursion(List<List<Integer>> li,List<Integer> temp,int i,int[] nums){
+        if(i==nums.length){
+            li.add(new ArrayList(temp));
             return;
         }
-        
-            list.add(nums[index]);
-            returnSubsetArray(arrList,index+1,nums,list);
-            if(list.size()>0){
-                list.remove(list.size()-1);
-            int newIndex=index+1;
-            while(newIndex<nums.length && nums[newIndex]==nums[index])newIndex++;
-            returnSubsetArray(arrList,newIndex,nums,list);
-        
+        temp.add(nums[i]);
+        recursion(li,temp,i+1,nums);
+        temp.remove(temp.size()-1);
+        int newIndex=i+1;
+        while(newIndex<nums.length && nums[newIndex]==nums[i])newIndex+=1;
+        recursion(li,temp,newIndex,nums);
     }
-}
 }
