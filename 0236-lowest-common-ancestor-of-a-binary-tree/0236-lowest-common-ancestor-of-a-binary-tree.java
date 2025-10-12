3aea46a0d7a4b8218713b36fd22eed06480b0965
vinayyy2106 <96223810+vinayyy2106@users.noreply.github.com>
2025-10-12 11:13:35 +0530

    Time: 7 ms (59.91%), Space: 44.9 MB (48.24%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.java
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.java
@@ -7,26 +7,19 @@
  *     TreeNode(int x) { val = x; }
  * }
  */
- //catch here was if we find p on left and could not find q on right so it means q is below and p is the common ancestor now so no need of searching beelow
 class Solution {
-    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q){
-        if(root==null){
-            return null;
-        }
-        if (root==p || root==q){
-            return root;
-        }
-        TreeNode left=lowestCommonAncestor(root.left,p,q);
-        TreeNode right=lowestCommonAncestor(root.right,p,q);
-        if(left==null && right!=null){
-            return right;
-        }
-        if(left!=null && right==null){
-            return left;
-        }
-        if(left!=null && right!=null){
-            return root;
-        }
-        return null;
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if(root==null)return root;
+        return helper(root,p,q);
+    }
+    public TreeNode helper(TreeNode root,TreeNode p,TreeNode q){
+        if(root==null)return null;
+        if(root==p || root==q)return root;
+        TreeNode left=helper(root.left,p,q);
+        TreeNode right=helper(root.right,p,q);
+        if(left==null && right==null)return null;
+        if(left==null && right!=null)return right;
+        if(left!=null && right==null)return left;
+        return root;
     }
 }
