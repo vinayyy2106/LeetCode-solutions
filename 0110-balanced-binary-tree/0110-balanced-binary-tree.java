1c3aa779a048b5eaa5464eb8cc92f33611c7838b
vinayyy2106 <96223810+vinayyy2106@users.noreply.github.com>
2025-10-12 09:55:35 +0530

    Time: 0 ms (100%), Space: 45.2 MB (8.11%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.java
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.java
@@ -15,30 +15,17 @@
  */
 class Solution {
     public boolean isBalanced(TreeNode root) {
-        if(root==null){
-            return true;
-        }
-        int val=checkBalanced(root);
-        if(val==-1){
-            return false;
-        }
-        return true;
+        if(root==null)return true;
+        int val=helper(root);
+        if(val==-1)return false;
+        else return true;
     }
-    public int checkBalanced(TreeNode root){
-        if(root==null){
-            return 0;
-        }
-        int lh=checkBalanced(root.left);
-        // if(lh==-1){
-        //     return -1;
-        // }
-        int rh=checkBalanced(root.right);
-        if(lh==-1 || rh==-1){
-            return -1;
-        }
-        if(Math.abs(lh-rh)>1){
-            return -1;
-        }
+    public int helper(TreeNode root){
+        if(root==null)return 0;
+        int lh=helper(root.left);
+        int rh=helper(root.right);
+        if(lh==-1 || rh==-1)return -1;
+        if(Math.abs(lh-rh)>1)return -1;
         return 1+Math.max(lh,rh);
     }
 }
