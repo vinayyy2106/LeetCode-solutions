a9d55afdf442700166814968534263c83772afe2
vinayyy2106 <96223810+vinayyy2106@users.noreply.github.com>
2025-12-10 20:25:37 +0530

    Time: 2 ms (74.67%), Space: 91.7 MB (42.67%) - LeetHub

---
+++ 3577-count-the-number-of-computer-unlocking-permutations/3577-count-the-number-of-computer-unlocking-permutations.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int countPermutations(int[] complexity) {
+        long MOD = 1_000_000_007;
+        int n=complexity.length;
+        long result = 1L;
+        for(int i=1;i<n;i++){
+            if(complexity[i]<=complexity[0]){
+                return 0;
+            }
+            result=(result * i) % MOD; // As answer was (n-1)! as at every palce we have the chance of having each element which comes after it in org. array..
+        }
+        return (int) (result);
+    }
+}
