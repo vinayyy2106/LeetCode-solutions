d71502af1904edc6fc7f1c1a14681727ed640f82
Vinay Kumar Pabba <96223810+vinayyy2106@users.noreply.github.com>
2026-01-02 19:09:15 +0530

    Time: 3 ms (48.08%), Space: 47 MB (94.15%) - LeetHub

---
+++ 0961-n-repeated-element-in-size-2n-array/0961-n-repeated-element-in-size-2n-array.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int repeatedNTimes(int[] nums) {
+        
+        int max=Integer.MIN_VALUE;
+        for(int num:nums){
+            max=Math.max(max,num);
+        }
+        int[] arr=new int[max+1];
+        for(int num:nums){
+            if(arr[num]>0){
+                return num;
+            }else{
+                arr[num]++;
+            }
+        }
+        return -1;
+    }
+}
