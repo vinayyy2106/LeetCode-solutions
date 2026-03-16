bfcf9b650afd92de0de9dab731d9f3de23a15a3b
Vinay Kumar Pabba <96223810+vinayyy2106@users.noreply.github.com>
2026-03-16 19:07:25 +0530

    Time: 0 ms (100%), Space: 45.2 MB (98.52%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.java
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.java
@@ -24,8 +24,9 @@ class Solution {
         if(root==null)return 0;
         int lh=helper(root.left);
         int rh=helper(root.right);
-        if(lh==-1 || rh==-1)return -1;
+        
         if(Math.abs(lh-rh)>1)return -1;
+        if(lh==-1 || rh==-1)return -1;
         return 1+Math.max(lh,rh);
     }
 }
