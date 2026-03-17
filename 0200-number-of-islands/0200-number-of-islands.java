3dd5b6dac61758613091a045a2679088886a884f
Vinay Kumar Pabba <96223810+vinayyy2106@users.noreply.github.com>
2026-03-17 08:10:53 +0530

    Time: 4 ms (45.41%), Space: 51.7 MB (81.07%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,34 +1,32 @@
 class Solution {
+    int n;
+    int m;
+    int[][] vis;
+    int[] delRow={-1,0,1,0};
+    int[] delCol={0,1,0,-1};
     public int numIslands(char[][] grid) {
-        int n=grid.length;
-        int m=grid[0].length;
-        int[][] vis=new int[n][m];
+        n=grid.length;
+        m=grid[0].length;
+        vis=new int[n][m];
         int count=0;
-        int[] delRow={-1,0,1,0};
-        int[] delCol={0,1,0,-1};
-        Queue<int[]> q=new LinkedList<>();
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(grid[i][j]=='1' && vis[i][j]!=1){
-                    vis[i][j]=1;
-                    q.offer(new int[]{i,j});
+                if(grid[i][j]=='1' && vis[i][j]==0){
                     count++;
-                    while(!q.isEmpty()){
-                        int[] arr=q.poll();
-                        int row=arr[0];
-                        int col=arr[1];
-                        for(int k=0;k<4;k++){
-                            int nrow=delRow[k]+row;
-                            int ncol=delCol[k]+col;
-                            if(nrow>=0 && nrow<n && ncol<m && ncol>=0 && vis[nrow][ncol]!=1 && grid[nrow][ncol]=='1'){
-                                vis[nrow][ncol]=1;
-                                q.offer(new int[]{nrow,ncol});
-                            }
-                        }
-                    }
+                    dfs(grid,i,j);
                 }
             }
         }
         return count;
+    }
+    public void dfs(char[][] grid,int i,int j){
+        vis[i][j]=1;
+        for(int k=0;k<4;k++){
+            int newRow=i+delRow[k];
+            int newCol=j+delCol[k];
+            if(newRow>=0 && newRow<n && newCol>=0 && newCol<m && vis[newRow][newCol]==0 && grid[newRow][newCol]=='1'){
+                dfs(grid,newRow,newCol);
+            }
+        }
     }
 }
