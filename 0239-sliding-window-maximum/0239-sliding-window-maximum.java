cb8e4ecebea77163d7998b271a9ffe211bffef6c
Vinay Kumar Pabba <96223810+vinayyy2106@users.noreply.github.com>
2026-03-18 18:54:08 +0530

    Time: 27 ms (95.5%), Space: 143.5 MB (87.42%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.java
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.java
@@ -1,28 +1,25 @@
 class Solution {
     public int[] maxSlidingWindow(int[] nums, int k) {
         int n=nums.length;
-        Deque<Integer> stack = new ArrayDeque<>();
-        for(int i=0;i<k;i++){
-            while(stack.size()>0 && nums[stack.peekLast()]<=nums[i]){
-                stack.pollLast();
-            }
-            stack.offerLast(i);
-        }
         int[] res=new int[n-k+1];
+        Deque<Integer> dq=new ArrayDeque<>();
+        for(int i=0;i<k;i++){
+            while(!dq.isEmpty() && nums[dq.peekLast()]<=nums[i]){
+                dq.pollLast();
+            }
+            dq.offerLast(i);
+        }
         int j=0;
-        res[j++]=nums[stack.peekFirst()];
-        for(int i=k;i<n;i++){
-            
-            //remove the elements not partof this window.
-             while(!stack.isEmpty() && stack.peekFirst()<=i-k){
-                stack.pollFirst();
+        res[j++]=nums[dq.peekFirst()];
+        for(int i=k;i<nums.length;i++){
+            while(!dq.isEmpty() && dq.peekFirst()<=i-k){
+                dq.pollFirst();
             }
-            while(!stack.isEmpty() && nums[stack.getLast()]<=nums[i]){
-                stack.pollLast();
+            while(!dq.isEmpty() && nums[dq.peekLast()]<=nums[i]){
+                dq.pollLast();
             }
-            
-            stack.offerLast(i);
-            res[j++]=nums[stack.peekFirst()];
+            dq.offerLast(i);
+            res[j++]=nums[dq.peekFirst()];
         }
         return res;
     }
