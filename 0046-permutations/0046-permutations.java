54f3cfa366a3639c5cf655cb22f278f52783bdaa
Vinay Kumar Pabba <96223810+vinayyy2106@users.noreply.github.com>
2026-03-18 11:13:30 +0530

    Time: 0 ms (100%), Space: 45.6 MB (42.39%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -1,25 +1,22 @@
 class Solution {
     public List<List<Integer>> permute(int[] nums) {
-        List<List<Integer>> lists=new ArrayList<>();
-        recursion(nums,lists,0);
-        return lists;
+        List<List<Integer>> list=new ArrayList<>();
+        helper(list,nums,0);
+        return list;
     }
-    public void recursion(int[] nums,List<List<Integer>> lists,int ind){
+    public void helper(List<List<Integer>> list,int[] nums,int ind){
         if(ind==nums.length){
             List<Integer> li=new ArrayList<>();
-            for(int num:nums){
-                li.add(num);
-            }
-            lists.add(li);
-            return;
+            for(int num:nums)li.add(num);
+            list.add(li);
         }
         for(int i=ind;i<nums.length;i++){
-            swap(nums,i,ind);
-            recursion(nums,lists,ind+1);
-            swap(nums,i,ind);
+            swap(nums,ind,i);
+            helper(list,nums,ind+1);
+            swap(nums,ind,i);
         }
     }
-    static void swap(int[] nums,int i,int j){
+    public void swap(int[] nums,int i,int j){
         int temp=nums[i];
         nums[i]=nums[j];
         nums[j]=temp;
