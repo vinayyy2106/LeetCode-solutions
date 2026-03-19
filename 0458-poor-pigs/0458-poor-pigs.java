ce3ad19bb91691d9cb7ed9b7632f8ea49dc30866
Vinay Kumar Pabba <96223810+vinayyy2106@users.noreply.github.com>
2026-03-19 23:32:36 +0530

    Time: 0 ms (100%), Space: 41.9 MB (82%) - LeetHub

---
+++ 0458-poor-pigs/0458-poor-pigs.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int poorPigs(int buckets, int minutesToDie, int minutesToTest) {
+        int trials=minutesToTest/minutesToDie;
+        int pigs=0;
+        while(Math.pow(trials+1,pigs)<buckets){
+            pigs++;
+        }
+        return pigs;
+    }
+}
