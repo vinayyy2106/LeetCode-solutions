0e66b3e5597bfb20d1b20e78a901868bde19306b
Vinay Kumar Pabba <96223810+vinayyy2106@users.noreply.github.com>
2026-08-02 12:50:18 +0530

    Time: 7 ms (21.79%), Space: 47 MB (10.85%) - LeetHub

--- 0877-stone-game/0877-stone-game.java
+++ 0877-stone-game/0877-stone-game.java
@@ -1,9 +1,22 @@
 class Solution {
     public boolean stoneGame(int[] piles) {
         int n=piles.length;
-        return solve(piles,0,n-1,false,0,0);
+        int sum=0;
+        for(int pile:piles){
+            sum+=pile;
+        }
+        int[][] dp=new int[n][n];
+        for(int[] arr:dp)Arrays.fill(arr,-1);
+        int aliceScore=solve(0,n-1,piles,dp);
+        return aliceScore>sum/2;
     }
-    public boolean solve(int[] piles,int i,int j,boolean aliceOrBob,int aliceScore,int bobScore){
-        return true;
+    public int solve(int i,int j,int[] piles,int[][] dp){
+        if(i>j){
+            return 0;
+        }
+        if(dp[i][j]!=-1)return dp[i][j];
+        int take_i=piles[i]+Math.min(solve(i+2,j,piles,dp),solve(i+1,j-1,piles,dp));
+        int take_j=piles[j]+Math.min(solve(i+1,j-1,piles,dp),solve(i,j-2,piles,dp));
+        return dp[i][j]=Math.max(take_i,take_j);
     }
 }
