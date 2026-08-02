e5a1677e0355f6e0ffae56b350b9e0e445cb6ece
Vinay Kumar Pabba <96223810+vinayyy2106@users.noreply.github.com>
2026-08-02 12:08:34 +0530

    Time: 0 ms (100%), Space: 43.2 MB (48.3%) - LeetHub

---
+++ 0877-stone-game/0877-stone-game.java
@@ -0,0 +1,9 @@
+class Solution {
+    public boolean stoneGame(int[] piles) {
+        int n=piles.length;
+        return solve(piles,0,n-1,false,0,0);
+    }
+    public boolean solve(int[] piles,int i,int j,boolean aliceOrBob,int aliceScore,int bobScore){
+        return true;
+    }
+}
